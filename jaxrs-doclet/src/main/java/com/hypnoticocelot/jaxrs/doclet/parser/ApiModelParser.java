7c72c89b90dfa1525e8b2931c136bf3cfeb8cfe7
stanasic <stanasic@gmail.com>
2014-09-28 14:58:20 +0200

    0.0.5-premer-SNAPSHOT: * Basic support for processing all class fields and field docs.

--- jaxrs-doclet/src/main/java/com/hypnoticocelot/jaxrs/doclet/parser/ApiModelParser.java
+++ jaxrs-doclet/src/main/java/com/hypnoticocelot/jaxrs/doclet/parser/ApiModelParser.java
@@ -2,6 +2,8 @@ package com.hypnoticocelot.jaxrs.doclet.parser;
 
 import com.google.common.base.Predicate;
 import com.google.common.base.Strings;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import com.hypnoticocelot.jaxrs.doclet.DocletOptions;
 import com.hypnoticocelot.jaxrs.doclet.model.Model;
 import com.hypnoticocelot.jaxrs.doclet.model.Property;
@@ -12,6 +14,12 @@ import java.util.*;
 
 import static com.google.common.collect.Collections2.filter;
 
+/**
+ * 
+ */
+/**
+ * 
+ */
 public class ApiModelParser {
 
     private final DocletOptions options;
@@ -40,7 +48,7 @@ public class ApiModelParser {
                         || startsWithAny(options.getPackagesToTreatAsOpaque(), type.qualifiedTypeName());
         ClassDoc classDoc = type.asClassDoc();
         if (isPrimitive || isJavaxType || isBaseObject || isTypeToTreatAsOpaque || classDoc == null) {
-            System.out.println("ApiModelParser.parseModel(): Skipping " + type.qualifiedTypeName());
+            // System.out.println("ApiModelParser.parseModel(): Skipping " + type.qualifiedTypeName());
             return;
         }
         if (alreadyStoredType(type)) {
@@ -48,12 +56,15 @@ public class ApiModelParser {
         }
         ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        Map<String, Type> types = findReferencedTypes(classDoc);
+        // Premer 2014-09-28: stanasic ////////////////////////////////////////////////////////////////////////////////////
+        Map<String, TypeHolder> types = findReferencedTypes(classDoc);
         Map<String, Property> elements = findReferencedElements(types);
         if (!elements.isEmpty()) {
             models.add(new Model(translator.typeName(type).value(), elements));
             parseNestedModels(types.values());
         }
+        
+        System.out.println("ApiModelParser.parseModel(): " + type.qualifiedTypeName());
     }
     
     // Premer 2014-07-28: stanasic ////////////////////////////////////////////////////////////////////////////////////
@@ -67,21 +78,34 @@ public class ApiModelParser {
     }
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    private Map<String, Type> findReferencedTypes(ClassDoc classDoc) {
-        Map<String, Type> elements = new HashMap<String, Type>();
+    // Premer 2014-09-28: stanasic ////////////////////////////////////////////////////////////////////////////////////
+    static class TypeHolder {
+        Type type;
+        String description;
+        TypeHolder(Type type, String description) {
+            super();
+            this.type = type;
+            this.description = description;
+        }
+    }
+    
+    private Map<String, TypeHolder> findReferencedTypes(ClassDoc classDoc) {
+        Map<String, TypeHolder> elements = Maps.newLinkedHashMap();
 
-        FieldDoc[] fieldDocs = classDoc.fields();
-        if (fieldDocs != null) {
-            for (FieldDoc field : fieldDocs) {
-                if (field.isStatic())
-                    continue;
+        // Premer 2014-09-28: stanasic ////////////////////////////////////////////////////////////////////////////////////
+        List<FieldDoc> fieldDocs = Lists.newArrayList();
+        allFields(classDoc, fieldDocs);
+        
+        for (FieldDoc field : fieldDocs) {
+            if (field.isStatic())
+                continue;
 
-                String name = translator.fieldName(field).value();
-                if (name != null && !elements.containsKey(name)) {
-                    elements.put(name, field.type());
-                }
+            String name = translator.fieldName(field).value();
+            if (name != null && !elements.containsKey(name)) {
+                elements.put(name, new TypeHolder(field.type(), field.getRawCommentText()));
             }
         }
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
         MethodDoc[] methodDocs = classDoc.methods();
         if (methodDocs != null) {
@@ -91,18 +115,29 @@ public class ApiModelParser {
 
                 String name = translator.methodName(method).value();
                 if (name != null && !elements.containsKey(name)) {
-                    elements.put(name, method.returnType());
+                    Tag[] rt = method.tags("@return");
+                    String rtc = (rt.length > 0) ? rt[0].text() : "";
+                    elements.put(name, new TypeHolder(method.returnType(), rtc));
                 }
             }
         }
         return elements;
     }
 
-    private Map<String, Property> findReferencedElements(Map<String, Type> types) {
+    private void allFields(ClassDoc classDoc, List<FieldDoc> fieldDocs) {
+        if (classDoc == null) {
+            return;
+        }
+        fieldDocs.addAll(Arrays.asList(classDoc.fields()));
+        allFields(classDoc.superclass(), fieldDocs);
+    }
+    
+    private Map<String, Property> findReferencedElements(Map<String, TypeHolder> types) {
         Map<String, Property> elements = new HashMap<String, Property>();
-        for (Map.Entry<String, Type> entry : types.entrySet()) {
+        for (Map.Entry<String, TypeHolder> entry : types.entrySet()) {
             String typeName = entry.getKey();
-            Type type = entry.getValue();
+            TypeHolder typeHolder = entry.getValue();
+            Type type = typeHolder.type;
             ClassDoc typeClassDoc = type.asClassDoc();
 
             Type containerOf = getTypeArgument(type);
@@ -111,19 +146,19 @@ public class ApiModelParser {
             String propertyName = translator.typeName(type).value();
             Property property;
             if (typeClassDoc != null && typeClassDoc.isEnum()) {
-                property = new Property(typeClassDoc.enumConstants(), null);
+                property = new Property(typeClassDoc.enumConstants(), typeHolder.description);
             } else {
-                property = new Property(propertyName, null, containerTypeOf);
+                property = new Property(propertyName, typeHolder.description, containerTypeOf);
             }
             elements.put(typeName, property);
         }
         return elements;
     }
 
-    private void parseNestedModels(Collection<Type> types) {
-        for (Type type : types) {
-            parseModel(type);
-            Type pt = getTypeArgument(type);
+    private void parseNestedModels(Collection<TypeHolder> types) {
+        for (TypeHolder type : types) {
+            parseModel(type.type);
+            Type pt = getTypeArgument(type.type);
             if (pt != null) {
                 parseModel(pt);
             }
